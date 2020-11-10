9202657475cb70c64c63194acf9159e202f55317
acmenes <acmenescompositions@gmail.com>
2020-11-10 18:14:32 -0500

    add files

---
+++ app.js
@@ -0,0 +1,24 @@
+function randomRGB(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
+
+const h1 = document.querySelector("h1");
+const letters = document.querySelectorAll(".letter")
+
+for(let letter of letters){
+    letter.style.color = randomRGB();
+}
+
+
+setInterval(function(){
+    for(let letter of letters){
+        letter.style.color = randomRGB();
+    }
+}, 500)
+
+
+
+
